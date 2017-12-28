3adbced03daca6227971b0d055e2872ced2f3a36
Alexandre <kavalerskialexandre@gmail.com>
2017-12-28 11:57:03 -0300

    Update and running CLI

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -4,11 +4,18 @@ const pkg = require('../package.json');
 
 const btcConverter = 'node.exe ./src/main.js';
 describe('Main CLI', () => {
-  it('should return version of btc-conv', (done) => {
+  it('should return version when btc-conv --version', (done) => {
     exec(`${btcConverter} --version`, (err, stdout, stderr) => {
       if (err) throw err;
       expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
       done();
-    })
+    });
+  });
+  it('should return the description when btc-conv --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.includes('A CLI to convert Bitcoin to any currency')).to.be.true;
+      done();
+    });
   });
 });
